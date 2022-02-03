087e92558ed80f00207c4cb863bbe20c404642a9
JSKitty <jskitty@protonmail.com>
2022-02-03 17:43:22 +0000

    Swap to faster SHA256 library
    Long-term I'd like to write an even more efficient one, however with the test code I've wrote in the past 12h, none of it was consistently higher performance than this (which I have no clue why not!), so I'll stick to this for now!

--- src/hasher.js
+++ src/hasher.js
@@ -1,7 +1,8 @@
 'use strict';
+const sha = require('sha.js').sha256;
 
 // Single SHA256 round
-const sha256 = require('@noble/hashes/sha256').sha256;
+const sha256 = (b) => (new sha).update(b).digest();
 
 // Double SHA256 round
 const sha256d = (b) => sha256(sha256(b));
